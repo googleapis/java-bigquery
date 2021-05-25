73aa479ed1d9661889f32170fe6431d64cb3feb0
Prashant Mishra <prash_mi@yahoo.com>
2021-05-25 15:55:03 +0530

    Extending BaseService to create BIGQUERY_EXCEPTION_HANDLER with retry

---
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryBaseService.java
@@ -0,0 +1,20 @@
+package com.google.cloud.bigquery;
+
+import com.google.cloud.ExceptionHandler;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.BaseService;
+
+abstract class BigQueryBaseService<OptionsT extends ServiceOptions<?, OptionsT>> extends BaseService{
+
+
+    protected BigQueryBaseService(ServiceOptions options) {
+        super(options);
+    }
+
+    public static final ExceptionHandler BIGQUERY_EXCEPTION_HANDLER =
+            ExceptionHandler.newBuilder()
+                    .abortOn(RuntimeException.class)
+                    .retryOn(java.net.ConnectException.class)//retry on Connection Exception
+                    .addInterceptors(EXCEPTION_HANDLER_INTERCEPTOR)
+                    .build();
+}
