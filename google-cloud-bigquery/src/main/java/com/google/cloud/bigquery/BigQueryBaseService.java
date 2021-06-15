ab0ef0bc319c0aea8739489d49b9fb3bc1f411da
Prashant Mishra <prash_mi@yahoo.com>
2021-06-15 21:39:08 +0530

    Fixed formatting

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryBaseService.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryBaseService.java
@@ -15,22 +15,22 @@
  */
 package com.google.cloud.bigquery;
 
+import com.google.cloud.BaseService;
 import com.google.cloud.ExceptionHandler;
 import com.google.cloud.ServiceOptions;
-import com.google.cloud.BaseService;
 
+abstract class BigQueryBaseService<OptionsT extends ServiceOptions<?, OptionsT>>
+    extends BaseService {
 
-abstract class BigQueryBaseService<OptionsT extends ServiceOptions<?, OptionsT>> extends BaseService{
+  protected BigQueryBaseService(ServiceOptions options) {
+    super(options);
+  }
 
-    protected BigQueryBaseService(ServiceOptions options) {
-        super(options);
-    }
-
-    public static final ExceptionHandler BIGQUERY_EXCEPTION_HANDLER =
-            ExceptionHandler.newBuilder()
-                    .abortOn(RuntimeException.class)
-                    .retryOn(java.net.ConnectException.class)//retry on Connection Exception
-                    .retryOn(java.net.UnknownHostException.class)//retry on UnknownHostException
-                    .addInterceptors(EXCEPTION_HANDLER_INTERCEPTOR)
-                    .build();
+  public static final ExceptionHandler BIGQUERY_EXCEPTION_HANDLER =
+      ExceptionHandler.newBuilder()
+          .abortOn(RuntimeException.class)
+          .retryOn(java.net.ConnectException.class) // retry on Connection Exception
+          .retryOn(java.net.UnknownHostException.class) // retry on UnknownHostException
+          .addInterceptors(EXCEPTION_HANDLER_INTERCEPTOR)
+          .build();
 }
