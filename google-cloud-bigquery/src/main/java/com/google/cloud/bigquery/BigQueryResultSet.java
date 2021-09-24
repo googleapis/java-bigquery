07016802fdd70e4cf1293ead3084be507a5aa3cb
Prashant Mishra <prash_mi@yahoo.com>
2021-09-24 12:47:00 +0530

    Added getResultSet method, removed unimplemented methods

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSet.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSet.java
@@ -16,7 +16,7 @@
 
 package com.google.cloud.bigquery;
 
-import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public interface BigQueryResultSet<T> {
 
@@ -29,12 +29,6 @@ public interface BigQueryResultSet<T> {
    */
   long getTotalRows();
 
-  /** Returns the next row. Null if there is no more rows left. */
-  // ResultSet getNext();
-
-  /*Advances the result set to the next row, returning false if no such row exists. Potentially blocking operation*/
-  boolean next() throws SQLException;
-
-  /*Returns the value of a String field if the field exists, otherwise returns null*/
-  String getString(String fieldName) throws SQLException;
+  /*Returns the The underlying ResultSet Implementation*/
+  ResultSet getResultSet();
 }
