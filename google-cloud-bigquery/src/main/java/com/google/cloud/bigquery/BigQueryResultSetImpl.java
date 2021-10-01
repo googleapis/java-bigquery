75f9a529c17cd9b7c8e9713c1df17ee26f0fb16f
Prashant Mishra <prash_mi@yahoo.com>
2021-10-01 14:27:22 +0530

    Fixed implementation and added more methods for BQ datatypes

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSetImpl.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSetImpl.java
@@ -18,8 +18,10 @@ package com.google.cloud.bigquery;
 
 import com.google.api.services.bigquery.model.TableRow;
 import java.math.BigDecimal;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.concurrent.BlockingQueue;
 
@@ -119,79 +121,111 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
 
     @Override
     public long getLong(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Long)) {
-        throw new SQLException("value not in instance of Long");
-      } else {
-        return Long.parseLong(String.valueOf(value));
       }
+      if (cursor == null) {
+        return 0l; // the column value; if the value is SQL NULL, the value returned is 0 as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        return ((FieldValueList) cursor).get(fieldName).getNumericValue().longValue();
+      }
+      return 0; // TODO: Implementation for Arrow
     }
 
     @Override
     public double getDouble(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Double)) {
-        throw new SQLException("value not in instance of Double");
-      } else {
-        return Double.parseDouble(String.valueOf(value));
       }
+      if (cursor == null) {
+        return 0d; // the column value; if the value is SQL NULL, the value returned is 0 as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        return ((FieldValueList) cursor).get(fieldName).getNumericValue().doubleValue();
+      }
+      return 0; // TODO: Implementation for Arrow
     }
 
     @Override
     public BigDecimal getBigDecimal(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Long
-          || value instanceof Double
-          || value instanceof BigDecimal
-          || value instanceof String)) {
-        throw new SQLException("value cannot be converted to BigDecimal");
-      } else {
-        return new BigDecimal(String.valueOf(value));
       }
+      if (cursor == null) {
+        return null; // the column value (full precision); if the value is SQL NULL, the value
+        // returned is null in the Java programming language. as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        return BigDecimal.valueOf(
+            ((FieldValueList) cursor).get(fieldName).getNumericValue().doubleValue());
+      }
+      return null; // TODO: Implementation for Arrow
     }
 
     @Override
     public boolean getBoolean(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Boolean || (value instanceof String))) {
-        throw new SQLException("value not in instance of Boolean");
-      } else {
-        return Boolean.parseBoolean(String.valueOf(value));
       }
+      if (cursor == null) {
+        return false; //  if the value is SQL NULL, the value returned is false
+      } else if (cursor instanceof FieldValueList) {
+        return ((FieldValueList) cursor).get(fieldName).getBooleanValue();
+      }
+      return false; // TODO: Implementation for Arrow
     }
 
     @Override
-    public byte getByte(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+    public byte[] getBytes(String fieldName) throws SQLException {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Byte || (value instanceof String))) {
-        throw new SQLException("value not in instance of Boolean");
-      } else {
-        return Byte.parseByte(String.valueOf(value));
       }
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        return ((FieldValueList) cursor).get(fieldName).getBytesValue();
+      }
+      return null; // TODO: Implementation for Arrow
     }
 
     @Override
     public Timestamp getTimestamp(String fieldName) throws SQLException {
-      Object value = getObject(fieldName);
-      if (value == null) {
+      if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
-      } else if (!(value instanceof Long
-          || value instanceof Timestamp
-          || value instanceof String)) {
-        throw new SQLException("value cannot be converted to Timestamp");
-      } else {
-        return Timestamp.valueOf(String.valueOf(value));
       }
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        return new Timestamp(((FieldValueList) cursor).get(fieldName).getTimestampValue());
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public Time getTime(String fieldName) throws SQLException {
+      if (fieldName == null) {
+        throw new SQLException("fieldName can't be null");
+      }
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        return new Time(((FieldValueList) cursor).get(fieldName).getTimestampValue());
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public Date getDate(String fieldName) throws SQLException {
+      if (fieldName == null) {
+        throw new SQLException("fieldName can't be null");
+      }
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        return new Date(((FieldValueList) cursor).get(fieldName).getTimestampValue());
+      }
+      return null; // TODO: Implementation for Arrow
     }
   }
 }
