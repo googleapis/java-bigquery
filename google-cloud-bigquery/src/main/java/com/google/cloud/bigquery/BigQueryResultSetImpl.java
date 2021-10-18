68dea7a1d550bf7811aa8280610c82ab168a836e
Prashant Mishra <prash_mi@yahoo.com>
2021-10-18 11:54:11 +0530

    Implemented columnIndex overloads

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSetImpl.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/BigQueryResultSetImpl.java
@@ -94,6 +94,17 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public Object getObject(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null;
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() == null ? null : fieldValue.getValue();
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public String getString(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -102,6 +113,17 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
         return null;
       } else if (cursor instanceof FieldValueList) {
         FieldValue fieldValue = ((FieldValueList) cursor).get(fieldName);
+        return fieldValue.getValue() == null ? null : fieldValue.getStringValue();
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public String getString(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null;
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
         return fieldValue.getValue() == null ? null : fieldValue.getStringValue();
       }
       return null; // TODO: Implementation for Arrow
@@ -123,6 +145,18 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public int getInt(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return 0; // the column value; if the value is SQL NULL, the value returned is 0 as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() == null ? 0 : fieldValue.getNumericValue().intValue();
+      }
+      return 0; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public long getLong(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -138,6 +172,18 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public long getLong(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return 0L; // the column value; if the value is SQL NULL, the value returned is 0 as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() == null ? 0L : fieldValue.getNumericValue().longValue();
+      }
+      return 0L; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public double getDouble(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -147,6 +193,18 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
         // java.sql.ResultSet definition
       } else if (cursor instanceof FieldValueList) {
         FieldValue fieldValue = ((FieldValueList) cursor).get(fieldName);
+        return fieldValue.getValue() == null ? 0d : fieldValue.getNumericValue().doubleValue();
+      }
+      return 0d; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public double getDouble(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return 0d; // the column value; if the value is SQL NULL, the value returned is 0 as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
         return fieldValue.getValue() == null ? 0d : fieldValue.getNumericValue().doubleValue();
       }
       return 0d; // TODO: Implementation for Arrow
@@ -171,6 +229,21 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public BigDecimal getBigDecimal(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null; // the column value (full precision); if the value is SQL NULL, the value
+        // returned is null in the Java programming language. as per
+        // java.sql.ResultSet definition
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() == null
+            ? null
+            : BigDecimal.valueOf(fieldValue.getNumericValue().doubleValue());
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public boolean getBoolean(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -185,6 +258,17 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public boolean getBoolean(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return false; //  if the value is SQL NULL, the value returned is false
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() != null && fieldValue.getBooleanValue();
+      }
+      return false; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public byte[] getBytes(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -193,6 +277,17 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
         return null; //  if the value is SQL NULL, the value returned is null
       } else if (cursor instanceof FieldValueList) {
         FieldValue fieldValue = ((FieldValueList) cursor).get(fieldName);
+        return fieldValue.getValue() == null ? null : fieldValue.getBytesValue();
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public byte[] getBytes(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
         return fieldValue.getValue() == null ? null : fieldValue.getBytesValue();
       }
       return null; // TODO: Implementation for Arrow
@@ -218,6 +313,22 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
     }
 
     @Override
+    public Timestamp getTimestamp(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return fieldValue.getValue() == null
+            ? null
+            : new Timestamp(
+                fieldValue.getTimestampValue()
+                    / 1000); // getTimestampValue returns time in microseconds, and TimeStamp
+        // expects it in millis
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
     public Time getTime(String fieldName) throws SQLException {
       if (fieldName == null) {
         throw new SQLException("fieldName can't be null");
@@ -226,30 +337,44 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
         return null; //  if the value is SQL NULL, the value returned is null
       } else if (cursor instanceof FieldValueList) {
         FieldValue fieldValue = ((FieldValueList) cursor).get(fieldName);
-        if (fieldValue.getValue() != null) {
-          // Time ranges from 00:00:00 to 23:59:59.99999. in BigQuery. Parsing it to java.sql.Time
-          String strTime = fieldValue.getStringValue();
-          String[] timeSplt = strTime.split(":");
-          if (timeSplt.length != 3) {
-            throw new SQLException("Can not parse the value " + strTime + " to java.sql.Time");
-          }
-          int hr = Integer.parseInt(timeSplt[0]);
-          int min = Integer.parseInt(timeSplt[1]);
-          int sec = 0, nanoSec = 0;
-          if (timeSplt[2].contains(".")) {
-            String[] secSplt = timeSplt[2].split("\\.");
-            sec = Integer.parseInt(secSplt[0]);
-            nanoSec = Integer.parseInt(secSplt[1]);
-          } else {
-            sec = Integer.parseInt(timeSplt[2]);
-          }
-          // LocalTime.of()
-          return Time.valueOf(LocalTime.of(hr, min, sec, nanoSec));
-        } else {
-          return null;
-        }
+        return getTime(fieldValue);
       }
       return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public Time getTime(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
+        return getTime(fieldValue);
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    private Time getTime(FieldValue fieldValue) throws SQLException {
+      if (fieldValue.getValue() != null) {
+        // Time ranges from 00:00:00 to 23:59:59.99999. in BigQuery. Parsing it to java.sql.Time
+        String strTime = fieldValue.getStringValue();
+        String[] timeSplt = strTime.split(":");
+        if (timeSplt.length != 3) {
+          throw new SQLException("Can not parse the value " + strTime + " to java.sql.Time");
+        }
+        int hr = Integer.parseInt(timeSplt[0]);
+        int min = Integer.parseInt(timeSplt[1]);
+        int sec = 0, nanoSec = 0;
+        if (timeSplt[2].contains(".")) {
+          String[] secSplt = timeSplt[2].split("\\.");
+          sec = Integer.parseInt(secSplt[0]);
+          nanoSec = Integer.parseInt(secSplt[1]);
+        } else {
+          sec = Integer.parseInt(timeSplt[2]);
+        }
+        return Time.valueOf(LocalTime.of(hr, min, sec, nanoSec));
+      } else {
+        return null;
+      }
     }
 
     @Override
@@ -261,6 +386,17 @@ public class BigQueryResultSetImpl<T> implements BigQueryResultSet<T> {
         return null; //  if the value is SQL NULL, the value returned is null
       } else if (cursor instanceof FieldValueList) {
         FieldValue fieldValue = ((FieldValueList) cursor).get(fieldName);
+        return fieldValue.getValue() == null ? null : Date.valueOf(fieldValue.getStringValue());
+      }
+      return null; // TODO: Implementation for Arrow
+    }
+
+    @Override
+    public Date getDate(int columnIndex) throws SQLException {
+      if (cursor == null) {
+        return null; //  if the value is SQL NULL, the value returned is null
+      } else if (cursor instanceof FieldValueList) {
+        FieldValue fieldValue = ((FieldValueList) cursor).get(columnIndex);
         return fieldValue.getValue() == null ? null : Date.valueOf(fieldValue.getStringValue());
       }
       return null; // TODO: Implementation for Arrow
