4aea35a261b1fa2b8c1f0a3467b9f88f39f4fa5d
stephwang <stephwang@google.com>
2021-12-29 11:54:32 -0500

    add test requirements

---
+++ google-cloud-bigquery/src/test/java/com/google/cloud/bigquery/ConnectionImplTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigquery;
+
+public class ConnectionImplTest {
+
+  // TODO: Add testNextPageTask()
+
+  // TODO: Add testParseDataTask()
+
+  // TODO: Add testPopulateBuffer()
+
+  // TODO: Add testGetQueryResultsFirstPage()
+
+  // TODO: Add testFastQuerySinglePage()
+
+  // TODO: Add testFastQueryMultiplePages() --> should exercise processQueryResponseResults() method
+
+  // TODO: Add testFastQueryLongRunning() --> should exercise getSubsequentQueryResultsWithJob()
+  // method
+
+  // TODO: Add testLegacyQuerySinglePage() --> should exercise createQueryJob() and
+  // getQueryResultsFirstPage()
+
+  // TODO: Add testLegacyQueryMultiplePages() --> should exercise processQueryResponseResults()
+  // method
+
+  // TODO: Add testQueryDryRun()
+
+  // TODO: Add testCancel()
+}
