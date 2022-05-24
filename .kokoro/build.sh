fc1e5fc518f8c5fbb2353264ba2bb5871923fdd8
Mridula Peddada <mpeddada@google.com>
2022-05-24 16:53:52 -0400

    bring back nightly-it

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -69,6 +69,17 @@ integration)
       verify
     RETURN_CODE=$?
     ;;
+nightly-it)
+    mvn -B ${INTEGRATION_TEST_ARGS} \
+          -ntp \
+          -Dtest=ITNightlyBigQueryTest \
+          -DtrimStackTrace=false \
+          -Dclirr.skip=true \
+          -Denforcer.skip=true \
+          -fae \
+          verify
+    RETURN_CODE=$?
+    ;;
 graalvm)
     # Run Unit and Integration Tests with Native Image
     mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
