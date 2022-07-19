6f3ad0a5f1a448ce94e0a3e41a617172d68dc8e5
Neenu Shaji <Neenu1995@users.noreply.github.com>
2022-07-19 17:07:56 -0400

    Update build.sh

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -82,7 +82,7 @@ nightly-it)
     ;;
 graalvm)
     # Run Unit and Integration Tests with Native Image
-    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test 
     RETURN_CODE=$?
     ;;
 graalvm17)
