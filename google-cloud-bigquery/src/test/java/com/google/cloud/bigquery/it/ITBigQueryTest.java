da3ff4c1e25f77222084270466813fcc03d08fbb
Neenu Shaji <Neenu1995@users.noreply.github.com>
2023-03-08 13:58:16 -0500

    chore: fix error message (#2561)

--- google-cloud-bigquery/src/test/java/com/google/cloud/bigquery/it/ITBigQueryTest.java
+++ google-cloud-bigquery/src/test/java/com/google/cloud/bigquery/it/ITBigQueryTest.java
@@ -4219,7 +4219,7 @@ public class ITBigQueryTest {
           statistics.getBiEngineStats().getBiEngineReasons().get(0).getCode(), "OTHER_REASON");
       assertEquals(
           statistics.getBiEngineStats().getBiEngineReasons().get(0).getMessage(),
-          "Query output to destination table is not supported.");
+          "Only SELECT queries without a destination table can be accelerated.");
     }
     assertNotNull(statistics.getQueryPlan());
   }
