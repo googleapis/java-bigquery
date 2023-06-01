ac25b5b50db9d222dc3c697c0c2fdd7aeeed2d8d
Neenu1995 <neenushaji@google.com>
2023-06-01 16:53:13 -0400

    Chore: add add samples for sessionID support in Load jobs

---
+++ samples/snippets/src/test/java/com/example/bigquery/LoadLocalFileInSessionIT.java
@@ -0,0 +1,78 @@
+package com.example.bigquery;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNotNull;
+
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.FormatOptions;
+import com.google.cloud.bigquery.LegacySQLTypeName;
+import com.google.cloud.bigquery.Schema;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoadLocalFileInSessionIT {
+  private final Logger log = Logger.getLogger(this.getClass().getName());
+  private String tableName;
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private PrintStream originalPrintStream;
+
+  private static final String BIGQUERY_DATASET_NAME = System.getenv("BIGQUERY_DATASET_NAME");
+
+  private static void requireEnvVar(String varName) {
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("BIGQUERY_DATASET_NAME");
+  }
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    originalPrintStream = System.out;
+    System.setOut(out);
+    tableName = "LOADLOCALFILETESTTABLE_" + UUID.randomUUID().toString().substring(0, 8);
+    Schema schema =
+        Schema.of(
+            Field.of("Name", LegacySQLTypeName.STRING),
+            Field.of("Age", LegacySQLTypeName.NUMERIC),
+            Field.of("Weight", LegacySQLTypeName.NUMERIC),
+            Field.of("IsMagic", LegacySQLTypeName.BOOLEAN));
+    CreateTable.createTable(BIGQUERY_DATASET_NAME, tableName, schema);
+  }
+
+  @After
+  public void tearDown() {
+    // Clean up
+    DeleteTable.deleteTable(BIGQUERY_DATASET_NAME, tableName);
+    // restores print statements in the original method
+    System.out.flush();
+    System.setOut(originalPrintStream);
+    log.log(Level.INFO, "\n" + bout.toString());
+  }
+
+  @Test
+  public void loadLocalFileInSession() throws IOException, InterruptedException {
+    Path csvPath = FileSystems.getDefault().getPath("src/test/resources", "bigquery_noheader.csv");
+    String sessionId = LoadLocalFileInSession.createSessionForLoading(BIGQUERY_DATASET_NAME, tableName, csvPath, FormatOptions.csv());
+    assertFalse(sessionId.isEmpty());
+    LoadLocalFileInSession.loadLocalFileInSession(BIGQUERY_DATASET_NAME, tableName, csvPath, FormatOptions.csv(), sessionId);
+    assertThat(bout.toString()).contains("Successfully loaded to Session");
+  }
+}
