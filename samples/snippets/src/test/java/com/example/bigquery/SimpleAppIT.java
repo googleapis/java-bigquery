1de8eb8540df9f120b7feb2e2c518ced238d3b7c
PhongChuong <phong.hoa.chuong@gmail.com>
2024-10-11 13:02:17 -0400

    Add requireEnvVar check

--- samples/snippets/src/test/java/com/example/bigquery/SimpleAppIT.java
+++ samples/snippets/src/test/java/com/example/bigquery/SimpleAppIT.java
@@ -40,6 +40,12 @@ public class SimpleAppIT {
   private PrintStream originalPrintStream;
   private static final String PROJECT_ID = requireEnvVar("GOOGLE_CLOUD_PROJECT");
 
+  private static void requireEnvVar(String varName) {
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+  }
+
   @BeforeClass
   public static void checkRequirements() {
     requireEnvVar("GOOGLE_CLOUD_PROJECT");
