fb370bbf4e087a9d9702cc22621cf720226f6771
PhongChuong <phong.hoa.chuong@gmail.com>
2025-02-03 10:11:30 -0500

    test: do not merge

--- samples/snippets/src/test/java/com/example/bigquery/QueryExternalSheetsPermIT.java
+++ samples/snippets/src/test/java/com/example/bigquery/QueryExternalSheetsPermIT.java
@@ -80,6 +80,7 @@ public class QueryExternalSheetsPermIT {
 
   @Test
   public void testQueryExternalSheetsPerm() {
+    // TODO(NOW): DO NOT MERGE
     String sourceUri =
         "https://docs.google.com/spreadsheets/d/1i_QCL-7HcSyUZmIbP9E6lO_T5u3HnpLe7dnpHaijg_E/edit?usp=sharing";
     Schema schema =
