2d12ffbff895d34875975b42e7bd695358dc41b5
PhongChuong <phong.hoa.chuong@gmail.com>
2025-02-18 12:35:11 -0500

    testing

--- samples/snippets/src/main/java/com/example/bigquery/CreateTable.java
+++ samples/snippets/src/main/java/com/example/bigquery/CreateTable.java
@@ -18,6 +18,7 @@ package com.example.bigquery;
 
 // [START bigquery_create_table]
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.BigQueryOptions;
