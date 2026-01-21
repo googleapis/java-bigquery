13ce325f6a31a7b929a12d7c4e315e9a92d3ce9b
Kirill Logachev <kirl@google.com>
2026-01-21 23:17:04 +0000

    gemini review

--- .kokoro/jdbc-release.sh
+++ .kokoro/jdbc-release.sh
@@ -13,40 +13,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -euo pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${DIR}/../google-cloud-bigquery-jdbc
+cd "${DIR}/../google-cloud-bigquery-jdbc"
 
 FOLDER=release
 DATE=$(date '+%Y-%m-%d')
-COMMIT=$(git rev-parse HEAD)
+COMMIT=$(git rev-parse --short HEAD)
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 BUCKET=gs://bq_devtools_release_private/drivers/jdbc
-NIGHTLY_BUILD_DESTINATION=${BUCKET}/nightly/${VERSION}/${DATE}
+NIGHTLY_BUILD_DESTINATION="${BUCKET}/nightly/${VERSION}/${DATE}"
 
 # All dependencies release
-mkdir -p ./${FOLDER}
-make docker-package-all-dependencies PACKAGE_DESTINATION=$(pwd)/${FOLDER}
+mkdir -p "./${FOLDER}"
+make docker-package-all-dependencies PACKAGE_DESTINATION="$(pwd)/${FOLDER}"
 JAR_FILE=$(find . -wholename "./${FOLDER}/*jar" -print -quit)
-JAR_NAME=$(basename ${JAR_FILE} .jar)-${DATE}-${COMMIT}
+JAR_NAME=$(basename "${JAR_FILE}" .jar)-${DATE}-${COMMIT}
 
-gsutil cp ${JAR_FILE} "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}.jar"
-rm -rf ${FOLDER}
+gsutil cp "${JAR_FILE}" "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}.jar"
+rm -rf "${FOLDER}"
 
 # All dependencies release - shaded
-mkdir -p ./${FOLDER}
-make docker-package-all-dependencies-shaded PACKAGE_DESTINATION=$(pwd)/${FOLDER}
+mkdir -p "./${FOLDER}"
+make docker-package-all-dependencies-shaded PACKAGE_DESTINATION="$(pwd)/${FOLDER}"
 JAR_FILE=$(find . -wholename "./${FOLDER}/*jar" -print -quit)
 
-gsutil cp ${JAR_FILE} "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}-shaded.jar"
-rm -rf ${FOLDER}
+gsutil cp "${JAR_FILE}" "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}-shaded.jar"
+rm -rf "${FOLDER}"
 
 # Thin release
-mkdir -p ./${FOLDER}
-make docker-package PACKAGE_DESTINATION=$(pwd)/${FOLDER}
+mkdir -p "./${FOLDER}"
+make docker-package PACKAGE_DESTINATION="$(pwd)/${FOLDER}"
 ZIP_FILE=$(find . -wholename "./${FOLDER}/*zip" -print -quit)
 
-gsutil cp ${ZIP_FILE} "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}.zip"
-rm -rf ${FOLDER}
+gsutil cp "${ZIP_FILE}" "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}.zip"
+rm -rf "${FOLDER}"
 
 # Update latest version
 gsutil cp "${NIGHTLY_BUILD_DESTINATION}/${JAR_NAME}.zip" "${BUCKET}/google-cloud-bigquery-jdbc-latest.zip"
